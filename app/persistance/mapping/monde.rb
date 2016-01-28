30ce4de0f5f272faf8dda4aedabe30fa0757b6d6
Emeric GAICHET <egaichet@snapp.fr>
2016-01-28 22:31:52 +0100

    Persistance des régions du monde

--- app/persistance/mapping/monde.rb
+++ app/persistance/mapping/monde.rb
@@ -4,17 +4,29 @@ module Persistance
       include Mongoid::Document
       field :_id, type: String
       field :options, type: Hash
-      # field :carte, type: CarteHexagonale
+      field :régions, type: Array
 
       class << self
         def depuis_le_domaine(monde)
           monde.génère_un_id if monde.id.nil?
-          new(_id: monde.id, options: monde.options)
+          new({
+            _id: monde.id,
+            options: monde.options,
+            régions: monde.liste_des_régions.map{ |région| {
+              latitude: région.position.latitude,
+              longitude: région.position.longitude,
+              biomes: région.biomes
+            }}
+          })
         end
       end
 
       def vers_le_domaine
         monde = Domaine::Monde.new(options)
+        régions.each do |région|
+          position = Domaine::Position::Point.new(région[:latitude], région[:longitude])
+          monde.carte.région_à_la_position(position).modifie_le_biome(*région[:biomes])
+        end
         monde.id = _id
         monde
       end
--- test/persistance/depot_monde_integration.rb
+++ test/persistance/depot_monde_integration.rb
@@ -16,12 +16,15 @@ module Persistance
 
     def test_peut_récupérer_un_monde
       monde = Domaine::Monde.new(taille: :grand)
+      région = monde.carte.une_région
+      région.modifie_le_biome(:prairie)
       @dépot.dépose!(monde)
 
       monde_récupéré = @dépot.récupère!(monde.id)
 
-      assert_equal monde.id, monde_récupéré.id
-      assert_equal monde.options[:taille], monde_récupéré.options[:taille]
+      assert_equal monde.id, monde_récupéré.id, "Le monde récupéré n'est pas le bon"
+      assert_equal monde.options[:taille], monde_récupéré.options[:taille], "La taille n'a pas été persistée"
+      assert monde_récupéré.carte.région_à_la_position(région.position).biome?(:prairie), "La région n'a pas été persistée"
     end
   end
 end
